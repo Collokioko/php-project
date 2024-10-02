3c269917907ff039791f703a7c549de370db6b91
Collins Muteti <mutetic6@gmail.com>
2024-10-02 22:55:52 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php 
+
+// echo "hello world";
+// print "hello again";
+
+//variables
+// $name = "james";
+// $age = 45;
+// echo var_dump ($age);
+
+//casting -changing data type
+// $age =45;
+// $age = (string) $age;
+// echo var_dump ($age);
+
+
+// // conditional statements
+// $age =65;
+
+// if ($age < 51) {
+//     echo "have a good day!";
+// }else{
+//     echo "Not authorized";
+// }
+
+
+// arrays
+$cars = array("Volvo", "BMW", "Toyota");
+print_r ($cars);
